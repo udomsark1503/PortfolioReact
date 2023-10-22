587eccea85cec3a4175c9d457f52fe85af133d5c
Udomsark Amnuaysookwong <udomsark1863@hotmail.com>
2023-10-22 16:55:37 +0700

    2023-10-22

--- src/components/SoftwareSkill.jsx
+++ src/components/SoftwareSkill.jsx
@@ -5,52 +5,52 @@ const SoftwareSkill = () => (
   <Card bordered={true} className="CardConfig2">
     <h2>SOFTWARE SKILL</h2>
     <Divider />
-    <Row>
-      <Col flex="1 1 20%" className="IconProgramming">
+    <Row style={{display:'flex', justifyContent:'center'}}>
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingW" src="MyPic/HTML5.png" alt="" />
       </Col>
-      <Col flex="1 1 20%" className="IconProgramming">
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingD" src="MyPic/css.png" alt="" />
       </Col>
-      <Col flex="1 1 20%" className="IconProgramming">
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingW" src="MyPic/javascript.png" alt="" />
       </Col>
-      <Col flex="1 1 20%" className="IconProgramming">
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingD" src="MyPic/python.png" alt="" />
       </Col>
-      <Col flex="1 1 20%" className="IconProgramming">
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingW" src="MyPic/c.png" alt="" />
       </Col>
 
-      <Col flex="1 1 20%" className="IconProgramming">
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingD" src="MyPic/Csharp.png" alt="" />
       </Col>
-      <Col flex="1 1 20%" className="IconProgramming">
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingW" src="MyPic/jquery.png" alt="" />
       </Col>
-      <Col flex="1 1 20%" className="IconProgramming">
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingD" src="MyPic/mysql.png" alt="" />
       </Col>
-      <Col flex="1 1 20%" className="IconProgramming">
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingD" src="MyPic/mongodb.png" alt="" />
       </Col>
-      <Col flex="1 1 20%" className="IconProgramming">
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingW" src="MyPic/PostGresSQL.png" alt="" />
       </Col>
 
-      <Col flex="1 1 20%" className="IconProgramming">
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingD" src="MyPic/nodejs.png" alt="" />
       </Col>
-      <Col flex="1 1 20%" className="IconProgramming">
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingW" src="MyPic/react.png" alt="" />
       </Col>
-      <Col flex="1 1 20%" className="IconProgramming">
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingD" src="MyPic/nextjs-icon-2048x1234-pqycciiu.png" alt="" />
       </Col>
-      <Col flex="1 1 20%" className="IconProgramming">
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingW" src="MyPic/Bootstrap.png" alt="" />
       </Col>
-      <Col flex="1 1 20%" className="IconProgramming">
+      <Col flex="0 1 20%" className="IconProgramming">
         <Image class="logoProgrammingD" src="MyPic/antDesignLogo.png" alt="" />
       </Col>
     </Row>
